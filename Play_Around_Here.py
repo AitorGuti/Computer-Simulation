6003771c7e4e2d375886a6a475c210812e220b19
aitorg88 <aitorg88@gmail.com>
2021-03-29 22:12:48 +0900

    Update Play_Around_Here.py

--- Play_Around_Here.py
+++ Play_Around_Here.py
@@ -4,16 +4,6 @@ Created on Tue Mar 23 21:01:23 2021
 
 @author: aitor
 """
-
-import numpy as np
-from numpy.linalg import norm
-import math as m
-from matplotlib import pyplot as plt
-from matplotlib.animation import FuncAnimation
-from scipy.constants import G
-from copy import deepcopy as dpcopy
-import pandas as pd
-
 from Final_Project_Code import Body
 from Final_Project_Code import Simulate
 
@@ -29,13 +19,15 @@ def main():
     for i in inputdata:
         b = Body(float(i[1]), [float(i[2]), float(i[3])], [float(i[4]), float(i[5])], i[6], float(i[7]))
         Body_list.append(b)
+        
+    #Please add a body to the simulation here
     
     Simulation = Simulate(Body_list,10000)    #Create Simulation Object with timestep of 10000
     Simulation.Display()
     
     #TPK = Simulation.Array_Energies(700)    #To create n by 5 array where columns represent Time, Tot energy, Pot energy, Kin energy, and Mars-Sat in order.
     #Simulation.Plot_Time_Total(TPK)  #To plot Total energy against time in days
-    #df = pd.DataFrame(TPK)     #To create dataframe from Time, tot, pot, kin
-    #df.to_csv("THE_ARRAY.csv", index=False)    # To write dataframe as csv
+    #df = pd.DataFrame(TPK)     #To create dataframe from the n by 5 array
+    #df.to_csv("Total Energy vs Time.csv", index=False)    # To write dataframe as csv
 
 main()
